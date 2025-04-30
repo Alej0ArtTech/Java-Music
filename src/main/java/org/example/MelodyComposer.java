e6b82c5e8db382f7b6af3a3db893590ee8b2b2c2
AlejoArtTech <honki.ponki.dance@gmail.com>
2025-04-30 01:27:02 -0600

    Agregando versión final

--- src/main/java/org/example/MelodyComposer.java
+++ src/main/java/org/example/MelodyComposer.java
@@ -13,22 +13,26 @@ public class MelodyComposer implements JMC {
 
     public static void main(String[] args) throws Exception {
         // Creamos el pool de notas
-
-
         NotePool pool = new NotePool(
-                new int[]{A2, C3, E3, A2}, new double[]{2, 2, 2, 2},
-                new int[]{A3, C4, E4, G4}, new double[]{0.5, 0.25, 1, 0.75},
-                new int[]{A4, C5, E5, G5, B5}, new double[]{0.25, 0.5, 0.75, 1, 1.5}
+                new int[]{A2, C3, E3, G2},               // Graves
+                new double[]{1, 2, 3},
+
+                new int[]{D3, F3, A3, B3},               // Medios
+                new double[]{0.25, 0.5, 1, 1.5},
+
+                new int[]{C5, E5, G5, D6},               // Agudos (AQUÍ estamos en las notas más agudas)
+                new double[]{0.25, 0.5, 0.75, 1}
         );
 
         // Generamos los compases (puedes crear varios de diferentes instrumentos)
-        Phrase compasBajo = generarCompases(16,pool); // Generamos un compás de 4/4 para Bajo
+        Phrase compasBajo = generarCompases(16, pool); // Generamos un compás de 4/4 para Bajo
         Phrase compasPiano = generarCompases(16, pool); // Generamos un compás de 4/4 para Piano
+        Phrase compasViolin = generarCompasesViolin(16, pool); // Generamos un compás de 4/4 para Violín (agudo)
 
         // Creamos la partitura
         Score score = new Score("Melodía con Compases");
 
-        // Creamos las partes para cada instrumento (Bajo y Piano)
+        // Creamos las partes para cada instrumento (Bajo, Piano y Violín)
         Part bajoPart = new Part("Bajo", ELECTRIC_PIANO, 0);
         bajoPart.add(compasBajo);
         score.add(bajoPart);
@@ -37,11 +41,15 @@ public class MelodyComposer implements JMC {
         pianoPart.add(compasPiano);
         score.add(pianoPart);
 
+        Part violinPart = new Part("Violín", VIOLIN, 2);  // Añadimos la parte para el violín
+        violinPart.add(compasViolin);
+        score.add(violinPart);
+
         // Escribimos el archivo MIDI
-        Write.midi(score, "melodia_con_compases.mid");
+        Write.midi(score, "melodia_con_compases_y_violin_agudo_solo2.mid");
 
         // Reproducir el archivo MIDI
-        playMidi("melodia_con_compases.mid", 124);
+        playMidi("melodia_con_compases_y_violin_agudo_solo2.mid", 124);
     }
 
     private static void playMidi(String file, int bpm) throws Exception {
@@ -75,13 +83,15 @@ public class MelodyComposer implements JMC {
                 NotePool.NoteData noteData;
                 if (rand.nextDouble() < 0.1) {
                     noteData = pool.randomBajoNote();
-                } else {
+                } else if (rand.nextDouble() < 0.5) {  // Asignamos algunas notas para el piano
                     noteData = pool.randomPianoNote();
+                } else {  // Para el violín, forzamos el rango agudo
+                    noteData = pool.randomViolinNote();  // Función para generar notas agudas para el violín
                 }
 
                 if (duracionTotal + noteData.duration <= duracionCompas) {
-                    if (noteData.pitch == -1) {
-                        compas.add(new Note(Note.REST, noteData.duration));
+                    if (noteData.pitch == Note.REST) {
+                        compas.add(new Note(Note.REST, noteData.duration));  // Usamos Note.REST directamente
                     } else {
                         compas.add(new Note(noteData.pitch, noteData.duration));
                     }
@@ -98,4 +108,35 @@ public class MelodyComposer implements JMC {
         return fraseFinal;
     }
 
+    // Método especial para generar notas del violín en un rango agudo
+    public static Phrase generarCompasesViolin(int numeroDeCompases, NotePool pool) {
+        Phrase fraseFinal = new Phrase();
+        Random rand = new Random();
+
+        for (int i = 0; i < numeroDeCompases; i++) {
+            Phrase compas = new Phrase();
+            double duracionTotal = 0.0;
+            double duracionCompas = 4.0;
+
+            while (duracionTotal < duracionCompas) {
+                NotePool.NoteData noteData = pool.randomViolinNote();  // Solo generamos notas agudas para el violín
+
+                if (duracionTotal + noteData.duration <= duracionCompas) {
+                    if (noteData.pitch == Note.REST) {
+                        compas.add(new Note(Note.REST, noteData.duration));
+                    } else {
+                        compas.add(new Note(noteData.pitch, noteData.duration));
+                    }
+                    duracionTotal += noteData.duration;
+                }
+            }
+
+            // Añadimos las notas al compás final
+            for (Note n : compas.getNoteArray()) {
+                fraseFinal.addNote(n);
+            }
+        }
+
+        return fraseFinal;
+    }
 }
--- src/main/java/org/example/ga/NotePool.java
+++ src/main/java/org/example/ga/NotePool.java
@@ -5,8 +5,7 @@ import java.util.*;
 
 /**
  * Contiene los conjuntos de notas y duraciones por voz.
- */
-public class NotePool {
+ */public class NotePool {
     private final int[] bajoPitches;
     private final double[] bajoDur;
     private final int[] pianoPitches;
@@ -26,59 +25,43 @@ public class NotePool {
         this.violinDur = violinDur;
     }
 
-    // Método para generar una nota aleatoria para el Bajo
     public NoteData randomBajoNote() {
-        boolean insertRest = rand.nextDouble() < 0.1; // 10% de probabilidad de silencio
-        if (insertRest) {
-            double dur = bajoDur[rand.nextInt(bajoDur.length)];
-            return new NoteData(Note.REST, dur); // Usamos Note.REST para el silencio
-        }
-        int pitch = bajoPitches[rand.nextInt(bajoPitches.length)];
-        double dur = bajoDur[rand.nextInt(bajoDur.length)];
-        return new NoteData(pitch, dur);
+        return randomNoteFrom(bajoPitches, bajoDur, 0.1); // 10% silencio
     }
 
-    // Método para generar una nota aleatoria para el Piano
     public NoteData randomPianoNote() {
-        boolean insertRest = rand.nextDouble() < 0.5; // 50% de probabilidad de silencio
-        if (insertRest) {
-            double dur = pianoDur[rand.nextInt(pianoDur.length)];
-            return new NoteData(Note.REST, dur); // Usamos Note.REST para el silencio
-        }
-        int pitch = pianoPitches[rand.nextInt(pianoPitches.length)];
-        double dur = pianoDur[rand.nextInt(pianoDur.length)];
-        return new NoteData(pitch, dur);
+        return randomNoteFrom(pianoPitches, pianoDur, 0.5); // 50% silencio
     }
 
-    // Método para generar una nota aleatoria para el Violín
     public NoteData randomViolinNote() {
-        boolean insertRest = rand.nextDouble() < 0.2; // 20% de probabilidad de silencio
-        if (insertRest) {
-            double dur = violinDur[rand.nextInt(violinDur.length)];
-            return new NoteData(Note.REST, dur); // Usamos Note.REST para el silencio
-        }
-        int pitch = violinPitches[rand.nextInt(violinPitches.length)];
-        double dur = violinDur[rand.nextInt(violinDur.length)];
+        return randomNoteFrom(violinPitches, violinDur, 0.2); // 20% silencio
+    }
+
+    private NoteData randomNoteFrom(int[] pitches, double[] durations, double silenceProbability) {
+        boolean insertRest = rand.nextDouble() < silenceProbability;
+        double dur = durations[rand.nextInt(durations.length)];
+        int pitch = insertRest ? Note.REST : pitches[rand.nextInt(pitches.length)];
         return new NoteData(pitch, dur);
     }
 
-    // Método para generar un compás de 4/4 con notas aleatorias
+    // Ahora incluye violín también y selecciona instrumento de forma aleatoria
     public List<NoteData> generarCompas4_4() {
         List<NoteData> compas = new ArrayList<>();
         double duracionTotal = 0.0;
-        double duracionCompas = 4.0; // 4 beats en total para el compás 4/4
+        double duracionCompas = 4.0;
 
-        // Generamos notas hasta que se llene el compás
         while (duracionTotal < duracionCompas) {
             NoteData noteData;
-            // Cambiar el instrumento aleatoriamente o elegir uno por defecto
-            if (rand.nextBoolean()) {
-                noteData = randomBajoNote();  // Escoge bajo aleatorio
-            } else {
-                noteData = randomPianoNote();  // Escoge piano aleatorio
+
+            // Elige instrumento aleatoriamente
+            int selector = rand.nextInt(3);
+            switch (selector) {
+                case 0 -> noteData = randomBajoNote();
+                case 1 -> noteData = randomPianoNote();
+                default -> noteData = randomViolinNote();
             }
 
-            // Aseguramos que la duración total no supere 4 beats
+            // Agrega solo si cabe dentro del compás
             if (duracionTotal + noteData.duration <= duracionCompas) {
                 compas.add(noteData);
                 duracionTotal += noteData.duration;
@@ -88,7 +71,6 @@ public class NotePool {
         return compas;
     }
 
-    // Clase interna que contiene las notas y duraciones
     public static class NoteData {
         public final int pitch;
         public final double duration;
@@ -99,4 +81,3 @@ public class NotePool {
         }
     }
 }
-
